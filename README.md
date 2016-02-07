c4a2a2c45ce2c948a07a30751d9d407185b5269c
Tomasz Klim <github@tomaszklim.pl>
2016-02-07 22:46:16 +0100

    first version

---
+++ README.md
@@ -0,0 +1,7 @@
+sf-log-monitor extension is a wrapper for logcheck, logwatch, or other
+syslog/systemd-journal monitoring solution, specific to the operating
+system distribution and version.
+
+Apart from just installing one of mentioned tools (currently only logcheck
+is supported, and only on Debian/Ubuntu), it provides a set of custom rules,
+which helps to minimize the amount of messages, that require manual review.
---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+. /opt/farm/scripts/init
+. /opt/farm/scripts/functions.custom
+. /opt/farm/scripts/functions.install
+
+
+
+base=/opt/sf-log-monitor/templates/$OSVER
+
+if [ ! -f $base/logcheck.tpl ]; then
+	echo "skipping logcheck setup, unsupported operating system version"
+	exit 1
+fi
+
+install_deb logcheck
+
+echo "setting up custom ignoring rules for logcheck"
+cp -f /opt/sf-log-monitor/rules/local-farmer.tpl /etc/logcheck/ignore.d.server/local-farmer
+
+echo -n "detecting report level: "
+if [ -d /etc/NetworkManager ]; then
+	level="workstation"
+else
+	level="server"
+fi
+echo $level
+
+echo "setting up logcheck configuration"
+file=/etc/logcheck/logcheck.conf
+save_original_config $file
+cat $base/logcheck.tpl |sed -e s/%%domain%%/`external_domain`/g -e s/%%level%%/$level/g >$file
+chown root:logcheck $file
+chmod 0640 $file
