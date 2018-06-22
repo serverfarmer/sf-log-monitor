ef6057f0477b820d284cb9d6204271fc6b392ca4
Tomasz Klim <github@tomaszklim.pl>
2018-06-22 22:42:10 +0200

    use sf-packages extension

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ if [ ! -f $base/logcheck.tpl ]; then
 	exit 1
 fi
 
-/opt/farm/ext/repos/utils/install.sh logcheck logcheck-database
+/opt/farm/ext/packages/utils/install.sh logcheck logcheck-database
 
 echo "setting up custom ignoring rules for logcheck"
 cp -f /opt/farm/ext/log-monitor/rules/local-farmer.tpl /etc/logcheck/ignore.d.server/local-farmer
